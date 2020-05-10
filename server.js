ee443246c0c2959db624b6b1adfe69b35cfd23c7
Proinfinto <furkanyaran@hotmail.com>
2020-05-10 21:24:54 +0300

    Server connection psql

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 var bodyParser = require('body-parser');
 const { Client } = require('pg');
 
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+  
+client.connect();
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -10,12 +17,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"));
 
-var data = '[' +
-'{ "id":"1" , "name":"Ömer" },' +
-'{ "id":"2" , "name":"Seçkin" },' +
-'{ "id":"3" , "name":"Furkan" } ]'; 
+client.connect(function(error) {
+    console.log("connection to db");
+    app.listen(PORT, function() {
+        console.log("listening at port " + PORT);
+    })
+};
 
-
-app.get('/api/all', (req, res) => res.json(data))
+app.get("/api/all", function(req,res) {
+    client.query("SELECT * FROM users;", function(error, result) {
+        res.json(result);
+    })
+})
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
