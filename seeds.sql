9b01122b16cbc860aa4c5a84a4d6fd727fa3de55
Proinfinto <furkanyaran@hotmail.com>
2020-05-10 20:38:30 +0300

    PosgtreSQL

--- seeds.sql
+++ seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (username,password,email)
-VALUES (koxphuman1,asder123,omerolkun@gmail.com),
-       (irb,123456,irb@gmail.com)
-       (Synthessia,123456,Synthessia@gmail.com)
+VALUES ('koxphuman1','asder123','omerolkun@gmail.com'),
+       ('irb','123456','irb@gmail.com')
+       ('Synthessia','123456','synthessia@gmail.com')
 
 
 
