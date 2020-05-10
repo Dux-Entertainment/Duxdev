7b1bf74c997b0ac6b742ff2cfacc6a874e6c7f93
Proinfinto <furkanyaran@hotmail.com>
2020-05-10 21:14:36 +0300

    fixed

--- seeds.sql
+++ seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (username,password,email)
+INSERT INTO users (username,password,email,created_on,last_login)
 VALUES ('koxphuman1','asder123','omerolkun@gmail.com','2020-05-10',null),
        ('irb','123456','irb@gmail.com','2020-05-10',null),
        ('Synthessia','123456','synthessia@gmail.com','2020-05-10',null)
