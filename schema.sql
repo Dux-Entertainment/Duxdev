4f5ae2eb454a40853ea318e47b3b2c56ba4b3527
Proinfinto <furkanyaran@hotmail.com>
2020-05-10 21:55:06 +0300

    Server connection psql

--- schema.sql
+++ schema.sql
@@ -3,6 +3,6 @@ CRATE TABLE users (
 	username VARCHAR (50) UNIQUE NOT NULL,
 	password VARCHAR (50) NOT NULL,
 	email VARCHAR (355) UNIQUE NOT NULL,
-	created_on TIMESTAMP NOT NULL,
+	created_on DATE,
 	last_login TIMESTAMP
 )
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
-var bodyParser = require('body-parser');
 const { Client } = require('pg');
+const Pool = require('pg').Pool
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
+
+const pool = new Pool({
+  user: 'sernpqbnsfvlul',
+  host: 'ec2-46-137-156-205.eu-west-1.compute.amazonaws.com',
+  database: 'd4g2n31slp06sk',
+  password: '18589cc69883b17ddcdd4129f1b98184581cd9d0b4f6e273728d7b6cc8aa84be',
+  port: 5432,
 });
+
   
 client.connect();
 
